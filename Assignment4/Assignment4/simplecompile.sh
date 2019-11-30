581dc7f084a93961dafface03401a5f376299558
Chan, Isaac Daniel <IsaacDaniel.Chan3@T-Mobile.com>
2019-11-29 17:18:56 -0800

    adding simplecompile.sh

--- Assignment4/Assignment4/simplecompile.sh
+++ Assignment4/Assignment4/simplecompile.sh
@@ -1,19 +1,3 @@
-#!/bin/bash
-
-# To more easily compile and run this program on CSS Linux Lab
-# Lines starting with '$' indicate what is typed on command line
-
-# if you get the following error:
-# -bash: ./simplecompile.sh: /bin/bash^M: bad interpreter: No such file or directory
-# run dos2unix to fix it
-# $ dos2unix simplecompile.sh
-
-# make this file executable
-# $ chmod 700 simplecompile.sh
-# redirect the output and stderr from this file to output.txt
-# $ ./simplecompile.sh > output.txt 2>&1
-
-
 date
 
 echo "*** compiling with clang++ to create an executable called myprogram"
