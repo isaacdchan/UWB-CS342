fca01ab919dca77760d10ce2966f6806b351f432
Chan, Isaac Daniel <IsaacDaniel.Chan3@T-Mobile.com>
2019-11-30 15:58:13 -0800

    fix memory errors pt. 2

--- Assignment5/simplecompile.sh
+++ Assignment5/simplecompile.sh
@@ -9,11 +9,11 @@ clang-tidy --version
 clang-tidy *.cpp -- -std=c++14
 
 echo "*** running myprogram"
-./myprogram BankTransIn.txt
+# ./myprogram BankTransIn.txt
 
 # valgrind will detect memory leaks
-# echo "*** running with valgrind"
-# valgrind ./myprogram BankTransIn.txtr
+echo "*** running with valgrind"
+valgrind --leak-check=full ./myprogram BankTransIn.txtr
 
 echo "*** cleaning up, deleting myprogram"
 rm myprogram
