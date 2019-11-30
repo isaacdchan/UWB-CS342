5667ff16aafb95826573bfc5792bc5bd3efdba9c
Chan, Isaac Daniel <IsaacDaniel.Chan3@T-Mobile.com>
2019-11-30 12:53:57 -0800

    fix LLVM errors pt. 1

--- Assignment5/main.cpp
+++ Assignment5/main.cpp
@@ -9,18 +9,18 @@ using namespace std;
 int main(int Argc, char *Argv[]) {
     Bank Bank;
 	
-	for (int i = 1; i < Argc; ++I) {
-		cout << "The command line argument(s) was " << Argv[i] << std::endl;
-		string FileName = Argv[i];
-		Bank.convertTxtToQueue(FileName);
-		Bank.processQueue();
-		Bank.displayErrors();
-	}
+	//for (int i = 1; i < Argc; ++i) {
+	//	cout << "The command line argument(s) was " << Argv[i] << std::endl;
+	//	string FileName = Argv[i];
+	//	Bank.convertTxtToQueue(FileName);
+	//	Bank.processQueue();
+	//	Bank.displayErrors();
+	//}
 
-	//Bank.convertTxtToQueue("C:/Users/isaac/Documents/UW Bothell/CS342/Assignment5/BankTransIn.txt");
-	//Bank.processQueue();
-	//Bank.displayAllBankBalances();
-	//Bank.displayErrors();
+	Bank.convertTxtToQueue("C:/Users/isaac/Documents/UW Bothell/CS342/Assignment5/BankTransIn.txt");
+	Bank.processQueue();
+	Bank.displayAllBankBalances();
+	Bank.displayErrors();
 
     cout << "Done" << endl;
     return 0;
