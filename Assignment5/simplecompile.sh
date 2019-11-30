e4e3156995c9391e9cc4f28f6ae3e0b6b0c67426
Chan, Isaac Daniel <IsaacDaniel.Chan3@T-Mobile.com>
2019-11-29 17:20:14 -0800

    origin master

---
+++ Assignment5/simplecompile.sh
@@ -0,0 +1,21 @@
+date
+
+echo "*** compiling with clang++ to create an executable called myprogram"
+clang++ --version
+clang++ -std=c++14 -Wall -Wextra -Wno-sign-compare *.cpp -g -o myprogram
+
+echo "*** running clang-tidy using options from .clang-tidy"
+clang-tidy --version
+clang-tidy *.cpp -- -std=c++14
+
+echo "*** running myprogram"
+./myprogram
+
+# valgrind will detect memory leaks
+echo "*** running with valgrind"
+valgrind ./myprogram
+
+echo "*** cleaning up, deleting myprogram"
+rm myprogram
+
+date
