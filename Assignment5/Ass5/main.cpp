e7c5a4a5d18181698c207553bb7ef4fb33d9e6c4
Isaac Chan <isaac.d.chan@gmail.com>
2019-12-03 20:39:30 -0800

    test

--- Assignment5/Ass5/main.cpp
+++ Assignment5/Ass5/main.cpp
@@ -9,19 +9,13 @@ using namespace std;
 int main(int Argc, char *Argv[]) {
     Bank Bank;
 	
-	//for (int i = 1; i < Argc; ++i) {
-	//	cout << "The command line argument(s) was " << Argv[i] << std::endl;
-	//	string FileName = Argv[i];
-	//	Bank.convertTxtToQueue(FileName);
-	//	Bank.processQueue();
-	//	Bank.displayErrors();
-	//}
-
-	string FileName = "BankTransIn.txt";
-	Bank.convertTxtToQueue(FileName);
-	Bank.processQueue();
-	Bank.displayAllBankBalances();
-	Bank.displayErrors();
+	for (int i = 1; i < Argc; ++i) {
+		cout << "The command line argument(s) was " << Argv[i] << std::endl;
+		string FileName = Argv[i];
+		Bank.convertTxtToQueue(FileName);
+		Bank.processQueue();
+		Bank.displayErrors();
+	}
 
     cout << "Done" << endl;
     return 0;
